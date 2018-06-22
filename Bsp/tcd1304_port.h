f73e4f70706fb6a5cd43ad96754870973b5fa580
Dung Do Dang <dung@neo.no>
2018-06-22 13:26:25 +0200

    Add skeleton for the portable layer of the TCD1304 sensor driver
    Prepare to split the hardware related softeare to a portable layer.
    This makes it easier to later migrate to different hardware platform,
    as only the portable layer is needed to be modified.

---
+++ Bsp/tcd1304_port.h
@@ -0,0 +1,51 @@
+/**
+ *******************************************************************************
+ * @file    : tcd1304_port.h
+ * @author  : Dung Do Dang
+ * @version : V1.0.0
+ * @date    : 2018-06-22
+ * @brief   : Portable layer of the driver for the CCD sensor chip from Toshiba
+ *
+ * The software module is split into two part.
+ * 1) Generic API to implement the necessary functionality to control the sensor
+ * 2) Portable layer to configure and control the hardware platform the sensor
+ * is connected to. In this way only (2) is needed to be changed if the hardware
+ * platform is needed to be replaced.
+ *
+ *******************************************************************************
+ *
+ * COPYRIGHT(c) 2018 Dung Do Dang
+ *
+ *******************************************************************************
+ */
+
+/**
+ ***************************** Revision History ********************************
+ * 2018-06-22 revision 0: Initial version
+ *
+ *******************************************************************************
+ */
+
+#ifndef TCD1304_PORT_H_
+#define TCD1304_PORT_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "tcd1304_conf.h"
+#include "stm32f7xx_hal.h"
+
+/* Exported typedefs ---------------------------------------------------------*/
+/* Exported defines ----------------------------------------------------------*/
+/* Exported macros -----------------------------------------------------------*/
+/* Exported variables --------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TCD1304_PORT_H_ */
