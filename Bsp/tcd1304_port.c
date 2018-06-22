6020d5c0dd3a26beb070d691ace6f60a20804fc8
Dung Do Dang <dung@neo.no>
2018-06-22 15:21:43 +0200

    Move ADC trigger source configuration to portable layer

--- Bsp/tcd1304_port.c
+++ Bsp/tcd1304_port.c
@@ -232,6 +232,99 @@ int32_t TCD_PORT_ConfigSHClock(const uint32_t intTime_us)
     return err;
 }
 
+/*******************************************************************************
+ * @brief
+ * @param
+ * @retval
+ *
+ ******************************************************************************/
+void TCD_PORT_ConfigADCTrigger(void)
+{
+    TIM_ClockConfigTypeDef sClockSourceConfig;
+    TIM_MasterConfigTypeDef sMasterConfig;
+    TIM_OC_InitTypeDef sConfigOC;
+    GPIO_InitTypeDef GPIO_InitStruct;
+    TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
+    uint32_t period = 4U * HAL_RCC_GetSysClockFreq() / CFG_FM_FREQUENCY_HZ - 1U;
+
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM8_CLK_ENABLE();
+
+    /* TIM8 GPIO Configuration. PC6------> TIM8_CH1 */
+    GPIO_InitStruct.Pin = ADC_TRIG_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF3_TIM8;
+    HAL_GPIO_Init( ADC_TRIG_GPIO_Port, &GPIO_InitStruct );
+
+    htim8.Instance = TIM8;
+    htim8.Init.Prescaler = 0;
+    htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim8.Init.Period = period;
+    htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim8.Init.RepetitionCounter = 0;
+    htim8.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+
+    if ( HAL_TIM_Base_Init( &htim8 ) != HAL_OK )
+    {
+        _Error_Handler( __FILE__, __LINE__ );
+    }
+
+    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+    if ( HAL_TIM_ConfigClockSource( &htim8, &sClockSourceConfig ) != HAL_OK )
+    {
+        _Error_Handler( __FILE__, __LINE__ );
+    }
+
+    if ( HAL_TIM_PWM_Init( &htim8 ) != HAL_OK )
+    {
+        _Error_Handler( __FILE__, __LINE__ );
+    }
+
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_OC1;
+    sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_ENABLE;
+
+    if ( HAL_TIMEx_MasterConfigSynchronization( &htim8, &sMasterConfig ) != HAL_OK )
+    {
+        _Error_Handler( __FILE__, __LINE__ );
+    }
+
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = period / 4U;
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+    sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+    sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+
+    if ( HAL_TIM_PWM_ConfigChannel( &htim8, &sConfigOC, TIM_CHANNEL_1 ) != HAL_OK )
+    {
+        _Error_Handler( __FILE__, __LINE__ );
+    }
+
+    sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+    sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+    sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+    sBreakDeadTimeConfig.DeadTime = 0;
+    sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+    sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+    sBreakDeadTimeConfig.BreakFilter = 0;
+    sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+    sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+    sBreakDeadTimeConfig.Break2Filter = 0;
+    sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+
+    if ( HAL_TIMEx_ConfigBreakDeadTime( &htim8, &sBreakDeadTimeConfig ) != HAL_OK )
+    {
+        _Error_Handler( __FILE__, __LINE__ );
+    }
+
+    HAL_TIM_PWM_Start( &htim8, TIM_CHANNEL_1 );
+    htim8.Instance->CR1 &= (~TIM_CR1_CEN);
+}
+
 /**
  *******************************************************************************
  *                        PRIVATE IMPLEMENTATION SECTION
