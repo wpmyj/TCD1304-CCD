ee6d918c82c6618a9413f907eb9f93259b0daad5
Dung Do Dang <dung@neo.no>
2018-06-25 11:46:36 +0200

    Return TCD_ERR_t error codes

--- Bsp/tcd1304.c
+++ Bsp/tcd1304.c
@@ -29,7 +29,7 @@ typedef struct
     uint16_t SensorData[ CFG_CCD_NUM_PIXELS ];
     uint16_t SensorDataAvg[ CFG_CCD_NUM_PIXELS ];
     uint32_t SensorDataAccu[ CFG_CCD_NUM_PIXELS ];
-    uint8_t  readyToRun;
+    uint8_t readyToRun;
     uint32_t specIndex;
     uint64_t totalSpectrumsAcquired;
 } TCD_PCB_t;
@@ -61,6 +61,8 @@ static TCD_ERR_t TCD_ADC_Init(void);
  ******************************************************************************/
 TCD_ERR_t TCD_Init(TCD_CONFIG_t *config)
 {
+    TCD_ERR_t err;
+
     if ( config == NULL )
     {
         return TCD_ERR_NULL_POINTER;
@@ -71,16 +73,32 @@ TCD_ERR_t TCD_Init(TCD_CONFIG_t *config)
     TCD_pcb.totalSpectrumsAcquired = 0U;
 
     /* Configure and start the ADC + DMA */
-    TCD_ADC_Init();
+    err = TCD_ADC_Init();
+    if ( err != TCD_OK )
+    {
+        return err;
+    }
 
     /* Configure the timers */
-    TCD_FM_Init();
-    TCD_ICG_Init();
-    TCD_SH_Init();
+    err = TCD_FM_Init();
+    if ( err != TCD_OK )
+    {
+        return err;
+    }
+    err = TCD_ICG_Init();
+    if ( err != TCD_OK )
+    {
+        return err;
+    }
+    err = TCD_SH_Init();
+    if ( err != TCD_OK )
+    {
+        return err;
+    }
 
     TCD_pcb.readyToRun = 1U;
 
-    return TCD_OK;
+    return err;
 }
 
 /*******************************************************************************
@@ -163,6 +181,8 @@ uint64_t TCD_GetNumOfSpectrumsAcquired(void)
  ******************************************************************************/
 static TCD_ERR_t TCD_FM_Init(void)
 {
+    TCD_ERR_t err = TCD_OK;
+
     if ( (TCD_config->f_master < 4000000U) && (TCD_config->f_master > 400000U) )
     {
         TCD_PORT_ConfigMasterClock( TCD_config->f_master );
@@ -170,7 +190,10 @@ static TCD_ERR_t TCD_FM_Init(void)
     else
     {
         TCD_PORT_ConfigMasterClock( CFG_FM_FREQUENCY_HZ );
+        err = TCD_WARN_FM;
     }
+
+    return err;
 }
 
 /*******************************************************************************
@@ -181,6 +204,8 @@ static TCD_ERR_t TCD_FM_Init(void)
  ******************************************************************************/
 static TCD_ERR_t TCD_ICG_Init(void)
 {
+    TCD_ERR_t err = TCD_OK;
+
     if ( (TCD_config->f_icg > 0U) && (TCD_config->f_icg <= 100U) )
     {
         TCD_PORT_ConfigICGClock( TCD_config->f_icg );
@@ -188,7 +213,10 @@ static TCD_ERR_t TCD_ICG_Init(void)
     else
     {
         TCD_PORT_ConfigICGClock( CFG_ICG_DEFAULT_FREQ_HZ );
+        err = TCD_WARN_ICG;
     }
+
+    return err;
 }
 
 /*******************************************************************************
@@ -199,6 +227,8 @@ static TCD_ERR_t TCD_ICG_Init(void)
  ******************************************************************************/
 static TCD_ERR_t TCD_SH_Init(void)
 {
+    TCD_ERR_t err = TCD_OK;
+
     uint32_t icg_period = 1000000U / TCD_config->f_icg;
 
     if ( (TCD_config->t_int_us > 10U) && (TCD_config->t_int_us < icg_period) )
@@ -208,9 +238,11 @@ static TCD_ERR_t TCD_SH_Init(void)
     else
     {
         TCD_PORT_ConfigSHClock( CFG_SH_DEFAULT_PERIOD_US );
+        err = TCD_WARN_SH;
     }
-}
 
+    return err;
+}
 
 /*******************************************************************************
  * @Brief   Generate the Master Clock to run at CFG_FM_FREQUENCY_HZ (TIM8)
@@ -220,16 +252,27 @@ static TCD_ERR_t TCD_SH_Init(void)
 static TCD_ERR_t TCD_ADC_Init(void)
 {
     /* Initialize the ADC hardware and DMA */
-    TCD_PORT_InitADC();
+    if ( TCD_PORT_InitADC() != 0 )
+    {
+        return TCD_ERR_ADC_INIT;
+    }
 
     /* Initialize the timer used to trigger AD conversion */
     TCD_PORT_ConfigADCTrigger();
 
+    /* Start the DMA transfer */
+    if ( TCD_PORT_StartADC( TCD_pcb.SensorData ) == 0 )
+    {
+        return TCD_OK;
+    }
+    else
+    {
+        return TCD_ERR_ADC_NOT_STARTED;
+    }
     /**
      * Start the DMA to move data from ADC to RAM.
      * From now on the AD conversion is controlled by hardware.
      */
-    TCD_PORT_StartADC( TCD_pcb.SensorData );
 }
 
 /****************************** END OF FILE ***********************************/
