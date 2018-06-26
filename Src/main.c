92e7075207c8d5bd71ab7ec000c86d215cf55c2b
Dung Do Dang <dung@neo.no>
2018-06-26 10:17:24 +0200

    Remove warning when compile with ARM Compiler version 5
    We are currently using ARM Compiler 6 (LLVM and CLang), but if someone is using
    ARMCC5 this removes the error.

--- Src/main.c
+++ Src/main.c
@@ -218,7 +218,7 @@ void _Error_Handler(char *file, int line)
     /* User can add his own implementation to report the HAL error return state */
     while ( 1 )
     {
-        __BKPT();
+        __BKPT( 0 );
     }
     /* USER CODE END Error_Handler_Debug */
 }
