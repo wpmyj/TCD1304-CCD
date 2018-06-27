wpmyj/TCD1304-CCD

commit 6b2ed596eed625c46c6140984b2cb520c93c7a5f

Add configurable ADC trigger frequency
Adjust the ADC trigger frequency to 1/4 of the master clock, which is configurable from
the user application.

Earlier this trigger frequency was set to 1/4 of the CFG_FM_FREQUENCY_HZ.
This can cause an unmatched frequency if the master clock is set to something else than
CFG_FM_FREQUENCY_HZ, defined in tcd1304_conf.h. To sanity check in later commits.

Bsp/tcd1304_port.c
@@ -301,14 +301,14 @@ int32_t TCD_PORT_ConfigSHClock(const uint32_t intTime_us)
  * @retval
  *
  ******************************************************************************/
-void TCD_PORT_ConfigADCTrigger(void)
+void TCD_PORT_ConfigADCTrigger(uint32_t Fs)
 {
     TIM_ClockConfigTypeDef sClockSourceConfig;
     TIM_MasterConfigTypeDef sMasterConfig;
     TIM_OC_InitTypeDef sConfigOC;
     GPIO_InitTypeDef GPIO_InitStruct;
     TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
-    uint32_t period = 4U * HAL_RCC_GetSysClockFreq() / CFG_FM_FREQUENCY_HZ - 1U;
+    uint32_t period = 4U * HAL_RCC_GetSysClockFreq() / Fs - 1U;
 
     /* Peripheral clock enable */
     __HAL_RCC_TIM8_CLK_ENABLE();

Bsp/tcd1304.c
@@ -305,7 +305,7 @@ static TCD_ERR_t TCD_ADC_Init(void)
     }
 
     /* Initialize the timer used to trigger AD conversion */
-    TCD_PORT_ConfigADCTrigger();
+    TCD_PORT_ConfigADCTrigger( TCD_config->f_master / 4U );
 
     /* Start the DMA transfer */
     if ( TCD_PORT_StartADC( TCD_pcb.data.SensorData ) == 0 )

Bsp/tcd1304_port.h
@@ -83,7 +83,7 @@ int32_t TCD_PORT_ConfigSHClock(const uint32_t integrationTime);
 int32_t TCD_PORT_ConfigICGClock(const uint32_t t_icg_us);
 
 int32_t TCD_PORT_InitADC(void);
-void    TCD_PORT_ConfigADCTrigger(void);
+void    TCD_PORT_ConfigADCTrigger(uint32_t Fs);
 int32_t TCD_PORT_StartADC(uint16_t *dataBuffer);
 
 /**

commit 74d3ffb577f3b959932e11197230070dfd8450f2

Bugfix, ADC trigger frequency is divided by 4 two times.
Once divided by 4 in TCD_ConfigADCTrigger(), it is not needed
in the TCD_PORT_ConfigADCTrigger().
This bug was introduced in commit: 6b2ed596eed625c46c6140984b2cb520c93c7a5f

Bsp/tcd1304_port.c
@@ -321,7 +321,7 @@ void TCD_PORT_ConfigADCTrigger(uint32_t Fs)
     TIM_OC_InitTypeDef sConfigOC;
     GPIO_InitTypeDef GPIO_InitStruct;
     TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
-    uint32_t period = 4U * HAL_RCC_GetSysClockFreq() / Fs - 1U;
+    uint32_t period = HAL_RCC_GetSysClockFreq() / Fs - 1U;
     timer_conf.Fs = Fs;
     
     /* Peripheral clock enable */